67c0f536c2c5869a614efeeffe2574e62f06a6a5
Sam VanderArk <vanderarks@brpsk12.org>
2017-02-21 20:23:01 -0500

    fsdghialbnjhl;'WRNKL"Sd

--- pisight.py
+++ pisight.py
@@ -6,29 +6,30 @@ import threading
 from time import sleep
 from time import time
 
-# Set to 1 to print output to the screen
-# Set to 0 to print output to the serial port
 DEBUG_MODE = 0
-DEBUG_PRINT = 10
-capId = 0
-lastCapId = 0
-frameCounter = 0
 
+per = 1000000
+countper = 0
+thingabob = per
 class MockSerial:
-	lastCapId = 0
-	lastFrameTime = 0
-	
 	def readline(self):
-		global capId
-		if self.lastCapId != capId:
-			self.lastFrameTime = capId - self.lastCapId
-			self.lastCapId = capId
-			return True
-		return False
+		global thingabob, per, countper
+		if countper >= 10:
+			countper = 0
+			return "the times they are\n".encode()
+		if thingabob:
+			thingabob -= 1
+			return "".encode()
+		else:
+			thingabob = per
+			countper += 1
+			return "boom\n".encode()
+		#return "waffles\n".encode()
 		
 	def write(self, data):
-		fps = 1 / self.lastFrameTime
-		print(str(fps) + "fps / " + str(self.lastFrameTime*1000) + "ms: " + data.decode())
+		pass
+		print( data.decode(), end="")
+
 
 if DEBUG_MODE:
 	ser = MockSerial()
@@ -55,6 +56,7 @@ DILATE_ITERATIONS = 0
 ERODE_ITERATIONS = 0
 
 cap = cv2.VideoCapture(0)
+
 frame = None
 
 lower_g = np.array([65, 100, 100])
@@ -65,17 +67,12 @@ data = ["", "", "", ""]
 def scrn21x(x):
 	return (x / SCREEN_X)*2-1
 def scrn21y(y):
-	return (y / SCREEN_Y)*-2+1
+	return (y / SCREEN_Y)*-2+1	#Hoo Ra
 
 def readImage():
-	global capId
-	global lastCapId
-	global DEBUG_PRINT
-	global frameCounter
 	global frame
-	
-	if frame == None:
-		return []
+	if frame is None:
+		return []	#Abandon all hope ye who enter here
 	
 	#ret, frame = cap.read() #gets frame from camera
 	#frame = cv2.blur(frame, (7,7))
@@ -97,61 +94,69 @@ def readImage():
 		area = rect[1][0]*rect[1][1]
 		areas.append(area)
 	actl = sorted(zip(rects, areas, contours), key=lambda x: x[1], reverse=True)
-	capId = time()
-	frameTime = capId - lastCapId
-	lastCapId = capId
-	frameCounter += 1
-	
-	if DEBUG_PRINT and frameCounter % DEBUG_PRINT == 0:
-		print(str(frameTime) + "/" + str(1/frameTime) + "\n")
 	
 	return actl
-
-def shootDistance_calculate(mid_y):
-	theta = FOV_Y / 2 * mid_y + REF_THETA
-	distance = (REF_TOW_H - REF_CAM_H) / math.tan(math.radians(theta))
-	return theta, distance
-'''def shootDistance_print(mid_x, mid_y, theta, distance):
-	print("mid_x" + str(mid_x))
-	print("mid_y" + str(mid_y))
-	print("theta" + str(theta))
-	print("Dist:" + str(distance),end="\n")'''
-def readDistance(): 
-	pass
-def readDistanceShooter():
-	while(True):
-		cl = readImage()
-		if( len(cl) > 0):
-			global data
-
-			topmost = scrn21y(tuple(cl[0][2][cl[0][2][:,:,1].argmin()][0])[1])	#I don't know how this works
-			bottommost = scrn21y(tuple(cl[0][2][cl[0][2][:,:,1].argmax()][0])[1]) #but the arrays are 4 deep
-			mid_x = scrn21x(cl[0][0][0][0])	#i know right
-
-			theta1, distance1 = shootDistance_calculate(topmost)
-			theta2, distance2 = shootDistance_calculate(bottommost)
-
-			data = [mid_x, scrn21y(cl[0][0][0][1]), (theta1+theta2)/2, (distance1+distance2)/2]
-		#else:
-		#	print("", end=".", flush=True)
-def readDistanceGears():
-	mid_x = (scrn21x(cl[0][0][0][0]) + scrn21x(cl[1][0][0][0]))/2
-	data = [mid_x, 0, 0, 0]	#don't use mid_y, theta, distance
-	#strafe instead
 
 def capImage():
 	global frame
 	while True:
 		ret, frame = cap.read() #gets frame from camera
 
-readDistance = readDistanceShooter
-def switch():
-		if readDistance == readDistanceShooter:
-			readDistance = readDistanceGears
+
+
+def shootDistance_calculate(mid_y):
+	theta = FOV_Y / 2 * mid_y + REF_THETA
+	distance = (REF_TOW_H - REF_CAM_H) / math.tan(math.radians(theta))
+	return theta, distance
+def readDistanceShooter():
+	cl = readImage()
+	if( len(cl) > 0):
+		global data
+
+		topmost = scrn21y(tuple(cl[0][2][cl[0][2][:,:,1].argmin()][0])[1])	#I don't know how this works
+		bottommost = scrn21y(tuple(cl[0][2][cl[0][2][:,:,1].argmax()][0])[1]) #but the arrays are 4 deep
+		mid_x = scrn21x(cl[0][0][0][0])	#i know right
+
+		theta1, distance1 = shootDistance_calculate(topmost)
+		theta2, distance2 = shootDistance_calculate(bottommost)
+
+		data = [mid_x, scrn21y(cl[0][0][0][1]), (theta1+theta2)/2, (distance1+distance2)/2]
+
+def readDistanceGears():
+	global data
+	cl = readImage()
+	if( len(cl) == 0):
+		return
+	elif len(cl) == 1:
+		mid_x = scrn21x(cl[0][0][0][0])
+	else:
+		mid_x = (scrn21x(cl[0][0][0][0]) + scrn21x(cl[1][0][0][0]))/2
+	data = [mid_x, 0, 0, 0]	#don't use
+
+CamState = "Shooter"	#vidcap default is 0
+SwitchState = False
+def readDistance():
+	global CamState, SwitchState, cap
+	if SwitchState:
+		global data
+		SwitchState = False
+		data = ["", "", "", ""]
+		if CamState == "Shooter":
+			cap = cv2.VideoCapture(1)
+			CamState = "Gears"
 		else:
-			readDistance = readDistanceShooter
+			cap = cv2.VideoCapture(0)
+			CamState = "Shooter"
+	if CamState == "Shooter":
+		readDistanceShooter()
+	else:
+		readDistanceGears()
 		
-		
+
+def readDistanceCaller():
+	while True:
+		readDistance()
+
 def checkGet():
 	while True:
 		global ser
@@ -159,53 +164,33 @@ def checkGet():
 		if ans:
 			global data
 			if ans.decode() == "the times they are\n":
-				switch()
+				global SwitchState
+				SwitchState = True
+				print("switching")
 			else:
 				#print("transmit")
 				ser.write(("sm" + str(data[0]) + "m" + str(data[1]) + "m" + str(data[2]) + "m" + str(data[3]) + "e\n").encode())
 
 def main():
-        threadArray = []
+	threadArray = []
 
-        capThread = FuncThread(capImage)
-        capThread.setDaemon(True)
-        capThread.start()
-        threadArray.append(capThread)
+	capThread = FuncThread(capImage)
+	capThread.setDaemon(True)
+	capThread.start()
+	threadArray.append(capThread)
 		
-        readThread = FuncThread(readDistance)
-        readThread.setDaemon(True)
-        readThread.start()
-        threadArray.append(readThread)
+	readThread = FuncThread(readDistanceCaller)
+	readThread.setDaemon(True)
+	readThread.start()
+	threadArray.append(readThread)
 
-        chkThread = FuncThread(checkGet)
-        chkThread.setDaemon(True)
-        chkThread.start()
-        threadArray.append(chkThread)
+	chkThread = FuncThread(checkGet)
+	chkThread.setDaemon(True)
+	chkThread.start()
+	threadArray.append(chkThread)
 
-        for thread in threadArray:
-            thread.join()
+	for thread in threadArray:
+ 		thread.join()
+
+
 main()
-#input("stop?")
-
-
-
-'''for i in cl:
-		for j in cl:
-			if (i == j):
-				continue
-			if abs(i[0][1] - j[0][1] - 0.1016) <= 0.1016 and abs(i[1][0] - j[1][0]) <= 0.2:
-				return i, j
-
-code for checking that they really are the tower vision tape'''
-
-'''
-			#mid_x = scrn21x(cl[0][0][0][0] + cl[0][0][1][0]/2)
-			#mid_y = -1*scrn21y(cl[0][0][0][1] + cl[0][0][1][1]/2)
-			#Reversed -count from top, not bottom
-'''
-
-'''if(abs(mid_x) >= SMALL_VAL):
-	print("mid_x" + str(mid_x))
-	turn(mid_x * FOV_X / 2)
-	continue
-'''
--- theRealCamSetup.sh
+++ theRealCamSetup.sh
@@ -1,3 +1,4 @@
+echo "--- Camera 1 ---"
 echo "***Brightness 30"
 v4l2-ctl --set-ctrl=brightness=30 --device=/dev/video0
 echo "Contrast 5"
@@ -24,3 +25,32 @@ echo "Tilt 0"
 v4l2-ctl --set-ctrl=tilt_absolute=0 --device=/dev/video0
 echo "Zoom 0"
 v4l2-ctl --set-ctrl=zoom_absolute=0 --device=/dev/video0
+
+echo "--- Camera 2 ---"
+echo "***Brightness 30"
+v4l2-ctl --set-ctrl=brightness=30 --device=/dev/video1
+echo "Contrast 5"
+v4l2-ctl --set-ctrl=contrast=5 --device=/dev/video1
+echo "**Saturation 100"
+v4l2-ctl --set-ctrl=saturation=100 --device=/dev/video1
+echo "***White Balance Temperature Auto 0"
+v4l2-ctl --set-ctrl=white_balance_temperature_auto=0 --device=/dev/video1
+echo "Power Line Frequency 2 (default don't touch)"
+v4l2-ctl --set-ctrl=power_line_frequency=2 --device=/dev/video1
+echo "***White Balance Temp 2800"
+v4l2-ctl --set-ctrl=white_balance_temperature=2800 --device=/dev/video1
+echo "Sharpness 25"
+v4l2-ctl --set-ctrl=sharpness=25 --device=/dev/video1
+echo "***Backlight_Compensation 0"
+v4l2-ctl --set-ctrl=backlight_compensation=0 --device=/dev/video1
+echo "***Exposure_Auto 1"
+v4l2-ctl --set-ctrl=exposure_auto=1 --device=/dev/video1
+echo "***Exposure Absolute 10"
+v4l2-ctl --set-ctrl=exposure_absolute=5 --device=/dev/video1
+echo "Pan 0"
+v4l2-ctl --set-ctrl=pan_absolute=0 --device=/dev/video1
+echo "Tilt 0"
+v4l2-ctl --set-ctrl=tilt_absolute=0 --device=/dev/video1
+echo "Zoom 0"
+v4l2-ctl --set-ctrl=zoom_absolute=0 --device=/dev/video1
+
